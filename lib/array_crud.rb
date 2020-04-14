03645b44b2b9a5dfd30f4168abae7595bfe4fd84
Tanattha Thuenim <tanattha.thuenim@gmail.com>
2020-04-14 15:14:31 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -20,15 +20,15 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array.shift(0)
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array.shift(1)
 end
 
 def retrieve_first_element_from_array(array)
-  
+   array.shift(0)
 end
 
 def retrieve_last_element_from_array(array)
